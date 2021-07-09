c2e93c5e895aa4aee2343cbe059c314c653a011c
Jeremiah <jsnidar@gmail.com>
2021-07-09 17:10:13 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+main.remove()
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory')
+const textToAdd = document.createTextNode("Jeremiah Snidar is the champion.")
+newHeader.appendChild(textToAdd)
+document.body.appendChild(h1);
