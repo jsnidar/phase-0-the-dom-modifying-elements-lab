5590539bc3c96db2c6bc8643b25f1146733d6e4d
Jeremiah <jsnidar@gmail.com>
2021-08-02 18:16:16 -0500

    commit tree b5ce6b4a947bd2176a908c028db9761ab28b45c1

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-// Write your code here!
-
 main.remove()
-
-const newHeader = document.createElement('h1');
-newHeader.setAttribute('id', 'victory')
-const textToAdd = document.createTextNode("Jeremiah Snidar is the champion.")
-newHeader.appendChild(textToAdd)
-document.body.appendChild(h1);
+const newHeader = document.createElement('h1')
+document.body.appendChild(newHeader);
+newHeader.id = 'victory'
+newHeader.textContent = "Jeremiah Snidar is the champion.";
